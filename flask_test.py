696733390cc865d728e1406a0b8e32e0c58a28d1
Chris Healy <chrishealy@Chriss-MacBook-Air.local>
2017-02-24 12:24:37 +0000

    Flask Link Injection Working

--- flask_test.py
+++ flask_test.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
+import json
 
 app = Flask(__name__)
 
 students = [
-    {"name": "Brendan", "Age": 30},
-    {"name": "Chris", "Age": 30},
-    {"name": "Mark", "Age": 30},
-    {"name": "Kathrin", "Age": 30},
-    {"name": "Colleen", "Age": 30},
-    {"name": "Tati", "Age": 30},
-    {"name": "Nikita", "Age": 30}
+    {"id": 1, "name": "Brendan", "age": 30},
+    {"id": 2, "name": "Chris", "age": 28},
+    {"id": 3, "name": "Kathrin", "age": 35},
+    {"id": 4, "name": "Colleen", "age": 39},
+    {"id": 5, "name": "Mark", "age": 40},
+    {"id": 6, "name": "Daniel", "age": 45},
+    {"id": 7, "name": "Tati", "age": 98},
 ]
 @app.route('/')
 def hello_world():
@@ -17,7 +18,7 @@ def hello_world():
 
 @app.route("/students")
 def show_Students():
-    return render_template("students.html")
+    return render_template("students.html", students = students)
 
 if __name__ == '__main__':
     app.run()
