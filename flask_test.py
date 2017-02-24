ad289072c8a12ab5be41cb51dff614b9d4aedf98
Chris Healy <chrishealy@Chriss-MacBook-Air.local>
2017-02-24 15:31:45 +0000

    Woking Flask Example with dynamic POST

--- flask_test.py
+++ flask_test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request,  render_template
 import json
 
 app = Flask(__name__)
@@ -16,9 +16,30 @@ students = [
 def hello_world():
     return render_template("index.html")
 
-@app.route("/students")
+@app.route('/students')
 def show_Students():
     return render_template("students.html", students = students)
+
+@app.route('/students/<int:id>')
+def show_a_student(id):
+    for student in students:
+        if student ['id'] == id:
+            return render_template("show_a_student.html",student = student)
+
+
+    return "Not Found"
+
+@app.route('/students/add', methods = ['GET', 'POST'])
+def add_a_student():
+    if request.method == 'POST':
+        id = len(students) + 1
+        name = request.form['Name']
+        age = request.form['Age']
+        students.append({"id": id, "name": name, "age": age})
+        return "OK"
+    else:
+        return render_template("add_a_student.html")
+
 
 if __name__ == '__main__':
     app.run()
